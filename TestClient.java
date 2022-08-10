fb7ed6b08da944abfab7b3239930b959d61e6a1b
jongwon-kr <76871947+jongwon-kr@users.noreply.github.com>
2022-08-10 15:13:17 +0900

    sendImage and drawFrame

--- TestClient.java
+++ TestClient.java
@@ -1,323 +1,51 @@
 package remoteConnect;
 
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.Robot;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.BufferedOutputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
-import javax.imageio.ImageIO;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JPanel;
-import javax.swing.JSplitPane;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-
-public class TestClient extends JFrame implements ActionListener, Runnable {
-	Robot r;
-	ServerSocket server_socket;
-	Socket socket_to_host;
+public class TestClient {
+	final int port_number = 7777;
 	String host_address = "127.0.0.1";
-	String s_local_address;
-	// server와 통신할 port번호
-	int port_to_host_number = 11332;
+	Socket socket = null;
 
-	ObjectInputStream ois = null;
-	static PrintWriter out = null;
-	String connectKey;
+	ObjectInputStream ois;
 
-	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	final int SCREEN_WIDTH = screenSize.width; // 화면 가로 너비
-	final int SCREEN_HEIGHT = screenSize.height; // 화면 세로 너비
-	static Image img = null;
-
-	JPanel top_panel; // 상단 패널
-	JButton connect; // 연결 버튼
-	JButton makeShareKey; // 공유키 생성
-	JTextField conTf; // 연결 textfield
-
-	JPanel centerLeft_panel; // 중앙 패널
-	JPanel centerRight_panel;
-	JSplitPane centerSplitPane; // 중앙 splitpane
-	JSplitPane mainSplitPane; // 메인화면 splitpane
-	JDialog dialog;
-
-	JMenuBar menubar;
-	JMenu menu;
-	JMenuItem server_ip;
-
-	boolean connectCheck = false;
-
-	public TestClient() {
-		super("원격 연결");
-		ConnectCreation();
-		getContentPane().add(setUI()).setBackground(Color.white);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(484, 363);
-		setVisible(true);
-	}
-
-	public int ConnectCreation() {
+	public void connecCreate() {
 		try {
-			socket_to_host = new Socket(host_address, port_to_host_number);
+			socket = new Socket(host_address, port_number);
 
-		} catch (UnknownHostException e) {
-			Alert("경고", "알수없는 호스트입니다.");
-			return 0;
-		} catch (IOException e) {
-			e.printStackTrace();
-			Alert("경고", "연결에 실패하였습니다.");
-			return 0;
+			ois = new ObjectInputStream(socket.getInputStream());
 
-		} // try-catch
-		connectCheck = true;
-		System.out.println("연결");
-		return 1;
-	}
-
-	public void Alert(String alert_title, String alert_message) {
-		// alert 메소드
-		dialog = new JDialog(this, alert_title, true);
-		JLabel lll = new JLabel(alert_message);
-		lll.setVerticalTextPosition(SwingConstants.CENTER);
-		lll.setHorizontalTextPosition(SwingConstants.CENTER);
-		JPanel ttt = new JPanel();
-		ttt.add(lll);
-		dialog.setLocation(180, 80);
-		dialog.setSize(320, 100);
-		dialog.setContentPane(ttt);
-		dialog.show();
-	}
-
-	// Main화면 UI설정
-	public Component setUI() {
-		top_panel = new JPanel(new FlowLayout());
-		centerLeft_panel = new JPanel();
-		centerRight_panel = new JPanel();
-
-		conTf = new JTextField();
-		conTf.setPreferredSize(new Dimension(100, 30));
-
-		// 연결 버튼
-		connect = new JButton("연결");
-		connect.setBackground(Color.white);
-		connect.setFocusable(false);
-		connect.setFont(new Font("Dialog", Font.BOLD, 12));
-		connect.setPreferredSize(new Dimension(60, 30));
-		connect.setActionCommand("connect");
-		connect.addActionListener(this);
-
-		// 공유키 생성 버튼
-		makeShareKey = new JButton("공유키 생성");
-		makeShareKey.setBackground(Color.white);
-		makeShareKey.setFocusable(false);
-		makeShareKey.setFont(new Font("Dialog", Font.BOLD, 12));
-		makeShareKey.setPreferredSize(new Dimension(120, 30));
-		makeShareKey.setActionCommand("makeShareKey");
-		makeShareKey.addActionListener(this);
-
-		centerLeft_panel.setPreferredSize(new Dimension(100, 300));
-		top_panel.setLayout(new FlowLayout(FlowLayout.LEFT));
-		top_panel.add(conTf);
-		top_panel.add(connect);
-		top_panel.add(makeShareKey);
-		centerSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, centerLeft_panel, centerRight_panel);
-		mainSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, top_panel, centerSplitPane);
-		return mainSplitPane;
-	}
-
-	// 이미지 그리기
-	public void drawImage(Image img, int x, int y) {
-		Graphics g = this.getGraphics();
-		g.drawImage(img, 0, 0, x, y, this);
-		this.paint(g);
-		this.repaint();
-	}
-
-	public void paint(Graphics g) {
-		if (RobotClient.img != null) {
-			g.drawImage(RobotClient.img, 0, 0, RobotClient.img.getWidth(this), RobotClient.img.getHeight(this), this);
-		}
-	}
-
-	public void run() {
-		BufferedImage image = null;
-		try {
-			while (true) {
-				if (ImageIO.read(ois) != null) {
-					System.out.println("not null");
-				}
-				if (image != null) {
-					int w = this.getWidth();
-					int h = this.getHeight();
-					image = (BufferedImage) image.getScaledInstance(w, h, Image.SCALE_DEFAULT);
-					drawImage(image, w, h);
-				} else {
-					System.out.println("못받음");
-				}
-			}
 		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}// run
 
-	// 접속요청한 화면 받아오기
-	class ReceiveScreen extends JFrame implements Runnable {
-		boolean onScreen = false;
-
-		public ReceiveScreen() {
-			super("접속화면");
-			onScreen = true;
-			setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-			setVisible(true);
-		}
-
-		public void run() {
-			Image image = null;
-			try {
-				while (onScreen) {
-					Thread.sleep(10);
-					out.println("#share#");
-					image = img;
-					if (image != null) { // image가 null이 아닌 경우
-						int w = this.getWidth();
-						int h = this.getHeight();
-						img = (Image) image.getScaledInstance(w, h, Image.SCALE_DEFAULT);
-						// this.repaint();
-						this.drawImage(img, w, h);
-					} else {
-						out.println("#share#");
-						System.out.println("이미지 못받음");
-					}
-					if (!isVisible())
-						onScreen = false;
-				}
-			} catch (Exception e) {
-
-			}
-		}
-
-		public void drawImage(Image img, int x, int y) {
-			Graphics g = this.getGraphics();
-			g.drawImage(img, 0, 0, x, y, this);
-			this.paint(g);
-			this.repaint();
-		}
-
-		public void paint(Graphics g) {
-			if (RobotClient.img != null) {
-				g.drawImage(RobotClient.img, 0, 0, RobotClient.img.getWidth(this), RobotClient.img.getHeight(this),
-						this);
-			}
 		}
 	}
 
-	public void actionPerformed(ActionEvent e) {
-		String command = e.getActionCommand();
-
-		if (command.equals("connect")) {
-			Thread t = new Thread(new ReceiveScreen());
-			t.start();
-		} else {
-			Alert("접속 실패", "원격접속에 실패했습니다.");
-		}
-	}
-
-	class SendMsg extends Thread {
-		Socket socket;
-		String msg;
-
-		FileOutputStream fos;
-		DataOutputStream dos;
-		BufferedOutputStream bos;
-
-		public SendMsg(Socket s, String msg) {
-			this.socket = s;
-			this.msg = msg;
-
-			try {
-				// 스트림 생성
-				dos = new DataOutputStream(socket.getOutputStream());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override
-		public void run() {
-			try {
-				dos.writeUTF("msg");
-				dos.flush();
-
-				dos.writeUTF(msg);
-				dos.flush();
-			} catch (IOException ioe) {
-				ioe.printStackTrace();
-			} finally {
-				try {
-					dos.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-
-	class SendFile extends Thread {
-		Socket socket;
+	class receiveScreen extends Thread {
 		Image img;
+		ObjectInputStream ois;
 
-		ObjectOutputStream oos;
+		public receiveScreen(ObjectInputStream ois) {
+			this.ois = ois;
 
-		SendFile(Socket s, Image i) {
-			this.socket = s;
-			this.img = i;
-
-			try {
-				oos = new ObjectOutputStream(socket.getOutputStream());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 
-		@Override
 		public void run() {
 			try {
-				oos.writeObject(img);
-				oos.flush();
+				img = null;
+				while (true) {
+					img = (Image) ois.readObject(this);
+
+					ois.close();
+				}
 			} catch (IOException e) {
-				e.printStackTrace();
+
 			}
 		}
 	}
 
 	public static void main(String[] args) {
-		TestClient tc = new TestClient();
-		tc.run();
 	}
-
 }
--- TestServer.java
+++ TestServer.java
@@ -1,223 +1,67 @@
 package remoteConnect;
 
-import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
-import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.Vector;
 
-import javax.imageio.ImageIO;
-import javax.swing.JFrame;
+public class TestServer extends Thread {
+	final int server_port = 7777;
+	ServerSocket ssock;
+	Socket csock = null;
 
-public class TestServer extends JFrame {
-
-	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	final int SCREEN_WIDTH = screenSize.width; // 화면 가로 너비
-	final int SCREEN_HEIGHT = screenSize.height; // 화면 세로 너비
-	final int server_port_number = 11332; // 서버 포트 넘버
-	Socket socket; // socket
-	ServerSocket server_socket; // server socket
-	Vector v_client_list; // 클라이언트 관리 리스트
-	static Image img = null; // 전송하는 화면
+	ObjectOutputStream oos;
 
 	public TestServer() {
-		// 클라이언트 수
-		v_client_list = new Vector();
-
-		// client가 network 상태인지 확인하기 위해 일정 간격으로 신호를 보내기 위한 timer 생성
-		Timer timer = new Timer();
-
-		// 2초마다 Check_client 객체 생성
-		timer.schedule(new Check_client(), 0, 2 * 1000);
 		try {
-			// 포트번호 12167에 SocketServer생성
-			server_socket = new ServerSocket(server_port_number);
-			while (true) {
-				try {
-					socket = server_socket.accept();
-					Connection c = new Connection(socket, this);
-					addClient(c);
-					// Connection class 가 가지고 있는 run메서드를 실행시켜 client와의 통신을 유지
-					c.start();
-				} catch (Exception e) {
-
-				}
+			ssock = new ServerSocket(server_port);
+			csock = ssock.accept();
+		} catch (IOException e) {
+			e.printStackTrace();
+			try {
+				csock.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
 			}
-		} catch (Exception e) {
-
 		}
 	}
 
-	// add client
-	public void addClient(Connection c) {
-		v_client_list.addElement(c);
-	}
-
-	public void capture() {
-		Robot robot;
+	public void run() {
 		try {
-			robot = new Robot();
-			Rectangle area = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-			img = robot.createScreenCapture(area); // Robot 클래스를 이용하여 스크린 캡쳐.
-		} catch (Exception e) {
+			Image img = null;
+			while ((csock = ssock.accept()) != null) {
+				img = capture();
+				oos = new ObjectOutputStream(csock.getOutputStream());
+				oos.writeObject(img);
+				oos.close();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	// removeClient 메소드 : network가 끊긴 client 삭제
-	public void removeClient(Connection c) {
-		// Check_client class가 client와 통신을 시도하다 실패하면 해당 socket을 닫는다.
-		c.closeSocket();
-
-		// client 목록에서 삭제
-		boolean b = v_client_list.remove(c);
-	}
-
-	class Connection extends Thread {
-		// client와 통신을 위해 만들어진 socket 이것에서 io를 뽑아낸다.
-		Socket socket;
-		// P2pServer의 class의 메서드를 사용하기 위해
-		TestServer robot_server;
-
-		// client에 들어온 메시지를 받기위해
-		BufferedReader in;
-
-		// client에게 메시지를 보내기위해
-		ObjectOutputStream oos; // 화면 전송 스트림
-
-		// Connection 생성자
-		public Connection(Socket s, TestServer t) {
-			this.socket = s;
-			this.robot_server = t;
-
-			try {
-
-				// inputStream 생성
-				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-				// outputStream 생성
-				oos = new ObjectOutputStream(socket.getOutputStream());
-			} catch (Exception e) {
-
-			}
+	public Image capture() {
+		Robot robot;
+		Image img = null;
+		try {
+			robot = new Robot();
+			Rectangle area = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+			img = robot.createScreenCapture(area); // Robot 클래스를 이용하여 스크린 캡쳐.
+			// this.repaint();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-
-		public void run() {
-			String msg = "";
-			// client로부터 메시지가 들어오기를 계속 대기
-			while (true) {
-				try {
-					// client로부터 메시지 한 줄 받기
-					msg = in.readLine();
-					System.out.println(msg);
-					if (msg != null) {
-						if (msg.startsWith("#share#")) {
-							capture();
-							if (img != null) {
-								oos.writeObject(img);
-								System.out.println("보냄");
-							} else {
-								System.out.println("못보냄");
-							}
-						}
-					} else {
-						System.out.println("no msg");
-					}
-				} catch (Exception e) {
-
-				}
-			}
-		}// run
-
-		// closeSocket
-		public void closeSocket() { // 사용된 io와 socket 닫기
-			try {
-				in.close();
-				oos.close();
-				socket.close();
-			} catch (Exception e) {
-
-			} // try-catch
-		}
-	} // end Connection Class
-
-	class Check_client extends TimerTask { // TimerTask에서 상속 받으면 run()메서드 override해야됨
-		public void run() {
-			BufferedImage img = null;
-			int client_size = v_client_list.size();
-
-			System.out.println("*****************************");
-			System.out.println("**      상태 check         **");
-			System.out.println("*****************************");
-			System.out.println("**    # of clients : " + client_size + "     **");
-			System.out.println("*****************************");
-
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			for (int i = 0; i < client_size; i++) {
-				try {
-					capture();
-					img = (BufferedImage) TestServer.img;
-					if (img != null) {
-						System.out.println("이미지 있음");
-					} else {
-						System.out.println("이미지 없음");
-					}
-					System.out.println(img.getWidth(null) + " " + img.getHeight(null));
-					SendFile sf = new SendFile(((Connection) v_client_list.elementAt(i)).socket, img);
-					sf.start();
-				} catch (Exception e) {
-					v_client_list.removeElementAt(i);
-					// 통신이 되지 않을 경우 목록에서 삭제
-				} // try-catch
-			} // for
-		} // run
-
-	} // Check_client Class
-
-	class SendFile extends Thread {
-		Socket socket;
-		BufferedImage img;
-
-		ObjectOutputStream oos;
-
-		SendFile(Socket s, BufferedImage i) {
-			this.socket = s;
-			this.img = i;
-
-			try {
-				oos = new ObjectOutputStream(socket.getOutputStream());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override
-		public void run() {
-			try {
-				oos.writeObject((Image) img);
-				oos.flush();
-
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+		return img;
 	}
 
 	// Main
 	public static void main(String[] args) {
-		new TestServer();
+		TestServer ts = new TestServer();
+		ts.start();
 	}
 }
